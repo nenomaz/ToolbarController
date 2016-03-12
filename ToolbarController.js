2aad8e5b76d333a2f52dfb4ec1637573b6891e3d
nenomaz <nazzarenomazzitello@gmail.com>
2016-03-12 16:18:11 +0100

    Applying Javascript prototype based programming

--- ToolbarController.js
+++ ToolbarController.js
@@ -21,75 +21,75 @@ function ToolbarController(name) {
 	this.name = name;
 	this.itemsSelected = [];
 
-	/*
-	* Add an item to the keeping track list
-	*/
-	this.addItem = function(value) {
-		this.itemsSelected.push(value);
-	};
-
-	/*
-	* Remove an item from the keeping track list
-	*/
-	this.removeItem = function(value) {
-		for(var i = 0; i < this.itemsSelected.length; i++) {
-			if (this.itemsSelected[i] == value)
-				this.itemsSelected.splice(i, 1);
-		}
-	};
-	
-	this.toggleItem = function(value) {
-		if (!this.hasItem(value)) {
-			this.addItem(value);
-		} else {
-			this.removeItem(value);
-		}
-	};
-
-	this.hasItem = function(value) {
-		var hasItem = false;
-		for(var i = 0; i < this.itemsSelected.length; i++) {
-			if (this.itemsSelected[i] == value) {
-				hasItem = true;
-				i = this.itemsSelected.length;
-			}
-		}
-		return hasItem;
-	};
-
-	/*
-	* Execute an action of the toolbar on the list items tracked
-	* @param action: an object with these properties: 
-	* 	string httpMethod
-	* 	string url
-	* 	bool ajax
-	*/
-	this.execute = function(action) {
-		if (!action.ajax) {
-			var form = this.getForm();
-			form.attr('method', action.httpMethod);
-			form.attr('action', action.url);
-			form.empty();
-			this.populateForm();
-			//form.submit();
-		}
-	};
-	
-	this.getForm = function() {
-		return jQuery("#" + this.name);
-	}
-	
-	this.populateForm = function() {
-		var form = this.getForm();
-		for (var i = 0; i < this.itemsSelected.length; i++) {
-			var hiddenField = '<input type="hidden" name="item[' + i + ']" value="' + this.itemsSelected[i] + '">';
-			form.append(hiddenField);
-		}
-	}
-
 	// This little snippet append a form to the body for non-Ajax actions
 	jQuery(function(){
 		var form = jQuery('<form id="' + name + '" style="display:none;">');
 		jQuery('body').append(form);
 	});
 };
+
+/*
+* Add an item to the keeping track list
+*/
+ToolbarController.prototype.addItem = function(value) {
+	this.itemsSelected.push(value);
+};
+
+/*
+* Remove an item from the keeping track list
+*/
+ToolbarController.prototype.removeItem = function(value) {
+	for(var i = 0; i < this.itemsSelected.length; i++) {
+		if (this.itemsSelected[i] == value)
+			this.itemsSelected.splice(i, 1);
+	}
+};
+
+ToolbarController.prototype.toggleItem = function(value) {
+	if (!this.hasItem(value)) {
+		this.addItem(value);
+	} else {
+		this.removeItem(value);
+	}
+};
+
+ToolbarController.prototype.hasItem = function(value) {
+	var hasItem = false;
+	for(var i = 0; i < this.itemsSelected.length; i++) {
+		if (this.itemsSelected[i] == value) {
+			hasItem = true;
+			i = this.itemsSelected.length;
+		}
+	}
+	return hasItem;
+};
+
+/*
+* Execute an action of the toolbar on the list items tracked
+* @param action: an object with these properties: 
+* 	string httpMethod
+* 	string url
+* 	bool ajax
+*/
+ToolbarController.prototype.execute = function(action) {
+	if (!action.ajax) {
+		var form = this.getForm();
+		form.attr('method', action.httpMethod);
+		form.attr('action', action.url);
+		form.empty();
+		this.populateForm();
+		form.submit();
+	}
+};
+
+ToolbarController.prototype.getForm = function() {
+	return jQuery("#" + this.name);
+}
+
+ToolbarController.prototype.populateForm = function() {
+	var form = this.getForm();
+	for (var i = 0; i < this.itemsSelected.length; i++) {
+		var hiddenField = '<input type="hidden" name="item[' + i + ']" value="' + this.itemsSelected[i] + '">';
+		form.append(hiddenField);
+	}
+}
